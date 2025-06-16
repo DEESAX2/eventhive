7c8f2fcbaa50337ed858dbbb15edd4c71efb277b
Alexykay <alexisayirebiacquah@gmail.com>
2025-06-16 10:01:11 -0700

    updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -54,7 +54,6 @@ export default function Register() {
             <button
                         className="w-full max-w-sm flex items-center justify-center border border-gray-300 py-2.5 rounded-md text-black hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50 transition-colors duration-200"
                     >
-                        {/* Replace with an actual Google SVG icon or image */}
                         <img src="https://www.svgrepo.com/show/303108/google-icon-logo.svg" alt="Google icon" className="w-5 h-5 mr-2" />Sign Up with Google</button>
              </div>
 
